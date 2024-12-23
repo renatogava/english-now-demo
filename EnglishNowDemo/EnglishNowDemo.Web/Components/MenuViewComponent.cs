f569806e62e52fbcd868f5592edccb3aac5fcbdf
Renato Gava <renatogava2@live.com>
2024-12-22 21:51:17 -0300

    menu

---
+++ EnglishNowDemo/EnglishNowDemo.Web/Components/MenuViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using EnglishNowDemo.Web.ViewModels.Menu;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishNowDemo.Web.Components
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var model = new MenuViewModel
+            {
+                PaginaAtual = ViewData["Menu"] != null ? (Menu)ViewData["Menu"] : Menu.Home
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ EnglishNowDemo/EnglishNowDemo.Web/ViewModels/Menu/MenuViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace EnglishNowDemo.Web.ViewModels.Menu
+{
+    public class MenuViewModel
+    {
+        public Menu PaginaAtual { get; set; }
+    }
+
+    public enum Menu
+    {
+        Home,
+        Professor,
+        Aluno
+    }
+}
